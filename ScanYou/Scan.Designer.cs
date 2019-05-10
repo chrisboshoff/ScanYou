6ff5313fc0557cf8b969eef991a1b8a88dcf7274
Chris Boshoff <chris.bossie@gmail.com>
2019-05-10 10:20:01 +0200

    initial

---
+++ ScanYou/Scan.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace ScanYou
+{
+    partial class Scan
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Scan));
+            this.listFound = new System.Windows.Forms.ListBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStripMenuItemExport = new System.Windows.Forms.ToolStripMenuItem();
+            this.ip1 = new System.Windows.Forms.TextBox();
+            this.ip2 = new System.Windows.Forms.TextBox();
+            this.ip3 = new System.Windows.Forms.TextBox();
+            this.ip4 = new System.Windows.Forms.TextBox();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.port = new System.Windows.Forms.TextBox();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblLastCheck = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listFound
+            // 
+            this.listFound.ContextMenuStrip = this.contextMenuStrip1;
+            this.listFound.FormattingEnabled = true;
+            this.listFound.Location = new System.Drawing.Point(21, 186);
+            this.listFound.Name = "listFound";
+            this.listFound.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.listFound.Size = new System.Drawing.Size(412, 264);
+            this.listFound.TabIndex = 0;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItemExport});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(170, 26);
+            // 
+            // toolStripMenuItemExport
+            // 
+            this.toolStripMenuItemExport.Name = "toolStripMenuItemExport";
+            this.toolStripMenuItemExport.Size = new System.Drawing.Size(169, 22);
+            this.toolStripMenuItemExport.Text = "Copy to clipboard";
+            this.toolStripMenuItemExport.Click += new System.EventHandler(this.toolStripMenuItemExport_Click);
+            // 
+            // ip1
+            // 
+            this.ip1.Location = new System.Drawing.Point(48, 46);
+            this.ip1.Name = "ip1";
+            this.ip1.Size = new System.Drawing.Size(58, 20);
+            this.ip1.TabIndex = 1;
+            this.ip1.Text = "192";
+            // 
+            // ip2
+            // 
+            this.ip2.Location = new System.Drawing.Point(112, 46);
+            this.ip2.Name = "ip2";
+            this.ip2.Size = new System.Drawing.Size(58, 20);
+            this.ip2.TabIndex = 2;
+            this.ip2.Text = "168";
+            // 
+            // ip3
+            // 
+            this.ip3.Location = new System.Drawing.Point(176, 46);
+            this.ip3.Name = "ip3";
+            this.ip3.Size = new System.Drawing.Size(58, 20);
+            this.ip3.TabIndex = 3;
+            this.ip3.Text = "0";
+            // 
+            // ip4
+            // 
+            this.ip4.Location = new System.Drawing.Point(240, 46);
+            this.ip4.Name = "ip4";
+            this.ip4.Size = new System.Drawing.Size(58, 20);
+            this.ip4.TabIndex = 4;
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(337, 46);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(84, 23);
+            this.btnGo.TabIndex = 5;
+            this.btnGo.Text = "Scan";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // port
+            // 
+            this.port.Location = new System.Drawing.Point(48, 80);
+            this.port.Name = "port";
+            this.port.Size = new System.Drawing.Size(80, 20);
+            this.port.TabIndex = 6;
+            this.port.Text = "80";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblStatus.Location = new System.Drawing.Point(21, 117);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(412, 19);
+            this.lblStatus.TabIndex = 7;
+            this.lblStatus.Text = "Status: Idle";
+            this.lblStatus.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "IP:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Port:";
+            // 
+            // lblLastCheck
+            // 
+            this.lblLastCheck.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblLastCheck.Location = new System.Drawing.Point(21, 141);
+            this.lblLastCheck.Name = "lblLastCheck";
+            this.lblLastCheck.Size = new System.Drawing.Size(412, 19);
+            this.lblLastCheck.TabIndex = 11;
+            this.lblLastCheck.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(24, 20);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(243, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Non-numeric input will be interpreted as a wildcard";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(20, 170);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Open ports:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(262, 170);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(171, 13);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Select items and right-click to copy";
+            // 
+            // Scan
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(456, 469);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblLastCheck);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.port);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.ip4);
+            this.Controls.Add(this.ip3);
+            this.Controls.Add(this.ip2);
+            this.Controls.Add(this.ip1);
+            this.Controls.Add(this.listFound);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Scan";
+            this.Text = "Scan";
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listFound;
+        private System.Windows.Forms.TextBox ip1;
+        private System.Windows.Forms.TextBox ip2;
+        private System.Windows.Forms.TextBox ip3;
+        private System.Windows.Forms.TextBox ip4;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.TextBox port;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblLastCheck;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemExport;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ ScanYou/Scan.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ScanYou
+{
+    public partial class Scan : Form
+    {
+        private bool stop = false;
+        private const string ButtonStart = "Scan";
+        private const string ButtonStop = "Stop";
+        public Scan()
+        {
+            InitializeComponent();
+            btnGo.Text = ButtonStart;
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            stop = btnGo.Text == ButtonStop;
+            btnGo.Text = btnGo.Text == ButtonStart ? ButtonStop : ButtonStart;
+
+            if (!stop)
+            {
+                listFound.Items.Clear();
+
+                var upper1 = LowerUpper(ip1.Text, out var lower1);
+                var upper2 = LowerUpper(ip2.Text, out var lower2);
+                var upper3 = LowerUpper(ip3.Text, out var lower3);
+                var upper4 = LowerUpper(ip4.Text, out var lower4);
+
+                Task.Run(() =>
+                {
+                    for (int x = lower1; (x <= upper1 && !stop); x++)
+                    {
+                        for (int y = lower2; (y <= upper2 && !stop) ; y++)
+                        {
+                            for (int i = lower3; (i <= upper3 && !stop) ; i++)
+                            {
+                                for (int j = lower4; (j <= upper4 && !stop) ; j++)
+                                {
+                                    string ip = $"{x}.{y}.{i}.{j}";
+                                    lblStatus.Invoke(new MethodInvoker(() =>
+                                    {
+                                        lblStatus.Text = $"Status: Checking {ip} : {port.Text}";
+                                        lblLastCheck.Text = $"Last Checked: {ip} : {port.Text}";
+                                        Application.DoEvents();
+                                    }));
+
+                                    if (IsPortOpen(ip, Convert.ToInt32(port.Text), new TimeSpan(0, 0, 0, 0, 5)))
+                                        listFound.Invoke(new MethodInvoker(() =>
+                                        {
+                                            listFound.Items.Add($"{ip} : {port.Text}");
+                                            Application.DoEvents();
+                                        }));
+                                }
+                            }
+                        }
+                    }
+
+                    lblStatus.Invoke(new MethodInvoker(() =>
+                    {
+                        lblStatus.Text = "Status: Idle";
+
+                        stop = false;
+                        btnGo.Text = ButtonStart;
+                    }));
+                });
+            }
+        }
+
+        private int LowerUpper(string value, out int lower)
+        {
+            int upper = 255;
+            if (int.TryParse(value, out lower))
+                upper = lower;
+            return upper;
+        }
+
+        private bool IsPortOpen(string host, int port, TimeSpan timeout)
+        {
+            try
+            {
+                using (var client = new TcpClient())
+                {
+                    var result = client.BeginConnect(host, port, null, null);
+                    var success = result.AsyncWaitHandle.WaitOne(timeout);
+                    if (!success)
+                    {
+                        return false;
+                    }
+
+                    client.EndConnect(result);
+                }
+            }
+            catch
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private void toolStripMenuItemExport_Click(object sender, EventArgs e)
+        {
+            string s = string.Empty;
+            foreach (object o in listFound.SelectedItems)
+            {
+                s += o + Environment.NewLine;
+            }
+
+            if (s != string.Empty)
+                Clipboard.SetText(s);
+        }
+    }
+}
